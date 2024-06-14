89ce9e3e79e3f8e65ec38ddfeb27ff7a2bf36068
Vitalii <crazzzyvio@gmail.com>
2024-06-14 17:44:03 +0300

    Maked component ListItem

---
+++ src/js/Components/ListItem/ListItem.js
@@ -0,0 +1,34 @@
+class ListItem {
+  state = (id, icon, comment, sum) => {
+    this.id = id;
+    this.icon = icon;
+    this.comment = comment;
+    this.sum = sum;
+
+    return this;
+  };
+
+  render = () => {
+    return (`
+    <li class="list__item item">
+      <div class="item__wrapper">
+        <div class="item__type" style="background-color: ${this.icon[1]}">
+          <img class="item__img" style="max-height: 40px; max-width: 40px;" src="./src/img/icons/${this.icon[0]}.png">
+        </div>
+        <p class="item__comment">${this.comment}</p>
+        <p class="item__price">
+          Sum: ${this.sum}
+        </p>
+      </div>
+      <button data-list-id="${this.id}" type="button" id="delete">
+        <svg style="pointer-events: none;" width="30px" height="30px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#000000"
+            d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z" />
+        </svg>
+      </button>
+    </li>`);
+  };
+}
+
+export default ListItem;
+
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 import "../style/style.scss";
 import MoneyTransactionManeger from "./transaction/MoneyTransactionManeger.js";
 import icons from "./config/icons.config.js";
+import ListItem from "./Components/ListItem/ListItem.js";
 
 let type;
 let comment;
@@ -10,6 +11,9 @@ let sum;
 const listItems = document.querySelector("#list");
 
 let moneyTransactionManeger = new MoneyTransactionManeger();
+let listItem = new ListItem();
+
+// listItem.render();
 
 function main() {
   document.querySelector(".form__button").addEventListener("click", async (event) => {
@@ -50,10 +54,8 @@ async function loadList() {
     const response = await moneyTransactionManeger.getTransactionsList();
 
     response.forEach(element => {
-      console.log(element);
-      listItems.innerHTML += listItem(element["id"], icons[element["type"]], element["comment"], element["price"]);
+      listItems.innerHTML += listItem.state(element["id"], icons[element["type"]], element["comment"], element["price"]).render();
     });
-    console.log("data loaded");
   } catch (error) {
     listItems.innerHTML += `<h1 style="text-align: center; font-size: 30px;">Sorry, ${error}</h1>`;
     throw new Error(error);
@@ -74,26 +76,6 @@ function dataValidityCheck(type, comment, sum) {
   }
 
   return [true, "data valid"];
-}
-
-function listItem(id, icon, comment, sum) {
-  return ` <li class="list__item item">
-  <div class="item__wrapper">
-    <div class="item__type" style="background-color: ${icon[1]}">
-      <img class="item__img" style="max-height: 40px; max-width: 40px;" src="./src/img/icons/${icon[0]}.png">
-    </div>
-    <p class="item__comment">${comment}</p>
-    <p class="item__price">
-      Sum: ${sum}
-    </p>
-  </div>
-  <button data-list-id="${id}" type="button" id="delete">
-    <svg style="pointer-events: none;" width="30px" height="30px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-      <path fill="#000000"
-        d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z" />
-    </svg>
-  </button>
-</li>`
 }
 
 loadList();
